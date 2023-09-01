ef54f9207245e9131d7de3f8b8f75aae2b1fe850
Icaro Rabelo <rabeloicarois@gmail.com>
2023-09-01 00:38:05 -0300

    chore(history): History Version Update: history@4.10.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
