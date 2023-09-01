0847b38912810f4997d16318e8dffabd48df987b
Icaro Rabelo <rabeloicarois@gmail.com>
2023-09-01 00:09:53 -0300

    feat(Router): React Router DOM configs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Login from './pages/Login/index';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 
+import Routes from './routes';
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { Nav } from './styled';
 
 export default function Header() {
   return (
     <Nav>
-      <a href="a">
+      <Link to="/">
         <FaHome size={24} />
-      </a>
-      <a href="b">
-        <FaUserAlt />
-      </a>
-      <a href="b">
+      </Link>
+      <Link to="Login">
+        <FaUserAlt size={24} />
+      </Link>
+      <Link to="teste">
         <FaSignInAlt size={24} />
-      </a>
+      </Link>
     </Nav>
   );
 }
