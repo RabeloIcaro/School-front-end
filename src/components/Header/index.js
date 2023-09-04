b5b22af9670ebf0f22816107e3022b8c9a18cb13
Icaro Rabelo <rabeloicarois@gmail.com>
2023-09-04 15:39:17 -0300

    feat(routes): Creating route pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const clickedButton = useSelector((state) => state.example.clickedButton);
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="Login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="teste">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {clickedButton ? 'Clicked' : 'Not clicked'}
     </Nav>
   );
 }
