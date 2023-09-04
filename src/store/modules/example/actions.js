2b0cce934355e7760167287ada1f1010599b2a61
Icaro Rabelo <rabeloicarois@gmail.com>
2023-09-04 14:25:58 -0300

    feat(Redux): Redux Saga configs

--- src/store/modules/example/actions.js
+++ src/store/modules/example/actions.js
@@ -1,7 +1,19 @@
 import * as types from '../types';
 
-export function buttonClick() {
+export function buttonClickRequest() {
   return {
-    type: types.CLICKED_BUTTON,
+    type: types.CLICKED_BUTTON_REQUEST,
+  };
+}
+
+export function buttonClickSuccess() {
+  return {
+    type: types.CLICKED_BUTTON_SUCCESS,
+  };
+}
+
+export function buttonClickFailure() {
+  return {
+    type: types.CLICKED_BUTTON_FAILURE,
   };
 }
