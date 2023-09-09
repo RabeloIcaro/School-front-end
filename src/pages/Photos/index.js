03b2d86f573b24d6cd8c9b100c8fa3da85cc4def
Icaro Rabelo <rabeloicarois@gmail.com>
2023-09-08 21:20:06 -0300

    feat: #time 30m (Photor): Student photo edition

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,11 +1,81 @@
 import React from 'react';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
+import Loading from '../../components/Loading';
 import { Container } from '../../styles/GlobalStyles';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Photos() {
+export default function Photos({ match }) {
+  const dispatch = useDispatch();
+  const id = get(match, 'params.id', '');
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [photo, setPhoto] = React.useState('');
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        setPhoto(get(data, 'Fotos[0].url', ''));
+        setIsLoading(false);
+      } catch {
+        toast.error('Error getting image ');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+    getData();
+  }, []);
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const photoURL = URL.createObjectURL(file);
+    setPhoto(photoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('foto', file);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/fotos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Your picture has been updated!');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response', '');
+      toast.error('Error getting picture');
+
+      if (status === 401) dispatch(actions.loginFailure);
+    }
+  };
   return (
     <Container>
-      <h1>Photos</h1>
+      <Loading isLoading={isLoading} />
+      <Title>Photos</Title>
+      <Form>
+        <label htmlFor="photo">
+          {photo ? (
+            <img src={photo} alt="" crossOrigin="anonymous" />
+          ) : (
+            'Select'
+          )}
+          <input type="file" id="photo" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Photos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
